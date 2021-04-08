3f717afc3118d9136d57656522aa21b2a76696df
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2021-04-08 18:36:11 -0300

    chore: adds the lobby config on server.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,2 @@
+export const DEFAULT_API_PORT = 8080;
 export const DEFAULT_PORT = 8000;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,18 @@
 import { Server } from 'boardgame.io/server';
-import { DEFAULT_PORT } from "../config/index.ts";
+import { DEFAULT_API_PORT, DEFAULT_PORT } from "../config/index.ts";
 
 // import SurfKingGame from './game'
 
 const PORT = Number(process.env.PORT || DEFAULT_PORT);
+const API_PORT = Number(process.env.API_PORT || DEFAULT_API_PORT);
 
 const server = Server({ games: [] });
 
-server.run(PORT, () => {
+const lobbyConfig = {
+    apiPort: API_PORT,
+    apiCallback: () => console.log(`Running Lobby API on port ${API_PORT}...`),
+};
+
+server.run({ port: PORT, lobbyConfig }, () => {
     console.log(`Serving at: http://localhost:${PORT}/`);
 })
