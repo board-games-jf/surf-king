38312d17a7d2ef2b36b4cc04df3e99859cff7bf8
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2021-04-22 19:23:55 +0000

    WIP 3

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-// declare module '*.module.css'
-
-// declare module '*.css' {
-//   //   const classes: { [key: string]: string }
-//   //   export default classes
-//   const content: string
-//   export default content
-// }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'next-auth/client'
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 
 import 'antd/dist/antd.css'
+import '../styles/globals.css'
 
 const App = ({ Component, pageProps }: AppProps): ReactNode => {
   const { session } = pageProps
