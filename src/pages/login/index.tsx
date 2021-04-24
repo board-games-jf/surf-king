5c19f2e1f730bbc31ca5fd8488ddffc00d026e95
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2021-04-24 09:19:33 -0300

    chore: adds the play mode buttons for 'medium' screen.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,23 @@
 import { Button, Col, Row } from 'antd'
 import Image from 'next/image'
+import { CSSProperties } from 'react'
 import Media from 'react-media'
 import { GLOBAL_MEDIA_QUERIES } from '../../app-constants'
 import Space from '../../components/space/style'
 import { intlValue, PLAY_MODE_LOCAL, PLAY_MODE_ONLINE } from '../../internationalization'
 
+const buttonStyle: CSSProperties = { height: 95, width: 356, fontSize: 30, fontWeight: 'bold' }
+const buttonContentStyle: CSSProperties = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '0 8px',
+}
+
 const Login = (): JSX.Element => {
+  const CrabImage = <Image src="/media/svg/crab.svg" width={70} height={70} />
+  const PenguimImage = <Image src="/media/svg/penguim.svg" width={80} height={80} />
+
   return (
     <Media queries={GLOBAL_MEDIA_QUERIES}>
       {(matches) => (
@@ -16,26 +28,57 @@ const Login = (): JSX.Element => {
           {!matches.small && (
             <>
               <Image src="/media/login-bg.png" alt="background" layout="fill" objectFit="cover" quality={100} />
-              <Row style={{ lineHeight: '100vh' }}>
-                <Col span={6} offset={5} style={{ textAlign: 'center' }}>
-                  <Button style={{ height: 95, fontSize: 30, fontWeight: 'bold', padding: '0 16px' }} type="primary" shape="round">
-                    <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
-                      <Image src="/media/svg/crab.svg" width={70} height={70} />
-                      <Space value={16} />
-                      {intlValue(PLAY_MODE_LOCAL)}
-                    </div>
-                  </Button>
-                </Col>
-                <Col span={6} offset={1} style={{ textAlign: 'center' }}>
-                  <Button style={{ height: 95, fontSize: 30, fontWeight: 'bold', padding: '0 16px' }} type="primary" shape="round">
-                    <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
-                      <Image src="/media/svg/penguim.svg" width={70} height={70} />
-                      <Space value={16} />
-                      {intlValue(PLAY_MODE_ONLINE)}
-                    </div>
-                  </Button>
-                </Col>
-              </Row>
+
+              {matches.medium && (
+                <div style={{ marginTop: 200 }}>
+                  <Row>
+                    <Col span={16} offset={4} style={{ textAlign: 'center' }}>
+                      <Button style={buttonStyle} type="primary" shape="round">
+                        <div style={buttonContentStyle}>
+                          {CrabImage}
+                          <Space value={16} />
+                          {intlValue(PLAY_MODE_LOCAL)}
+                        </div>
+                      </Button>
+                    </Col>
+                  </Row>
+                  <div style={{ marginTop: 16 }} />
+                  <Row>
+                    <Col span={16} offset={4} style={{ textAlign: 'center' }}>
+                      <Button style={buttonStyle} type="primary" shape="round">
+                        <div style={buttonContentStyle}>
+                          {PenguimImage}
+                          <Space value={16} />
+                          {intlValue(PLAY_MODE_ONLINE)}
+                        </div>
+                      </Button>
+                    </Col>
+                  </Row>
+                </div>
+              )}
+
+              {matches.large && (
+                <Row style={{ lineHeight: '100vh' }}>
+                  <Col span={6} offset={5} style={{ textAlign: 'center' }}>
+                    <Button style={buttonStyle} type="primary" shape="round">
+                      <div style={buttonContentStyle}>
+                        {CrabImage}
+                        <Space value={16} />
+                        {intlValue(PLAY_MODE_LOCAL)}
+                      </div>
+                    </Button>
+                  </Col>
+                  <Col span={6} offset={2} style={{ textAlign: 'center' }}>
+                    <Button style={buttonStyle} type="primary" shape="round">
+                      <div style={buttonContentStyle}>
+                        {PenguimImage}
+                        <Space value={16} />
+                        {intlValue(PLAY_MODE_ONLINE)}
+                      </div>
+                    </Button>
+                  </Col>
+                </Row>
+              )}
             </>
           )}
         </>
